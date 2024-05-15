569a6e36e0e4cb7d4587f7109dd10112d91286a6
DavidDCG <110427142+DavidDCG@users.noreply.github.com>
2024-05-15 15:20:25 -0600

    cambios correo
    Cambios correo

--- src/controllers/email_controller.js
+++ src/controllers/email_controller.js
@@ -2,44 +2,37 @@ const { dataReturn } = require('../helpers/constants');
 const nodemailer = require('nodemailer');
 
 const SendEmail = async (req, res) => {
-  
-
+  // Verifica si el cuerpo de la solicitud contiene los datos necesarios
+  if (!req.body.envio || !req.body.correo) {
+    return res.status(400).json({ error: 'Los datos de envío y el correo electrónico son obligatorios.' });
+  }
 
   try {
-    
-  
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // Use `true` for port 465, `false` for all other ports
-        auth: {
-          user: "halconet@tractozone.com.mx",
-          pass: "tracto2017",
-        },
-      });
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: 'halconet@tractozone.com.mx',
+        pass: 'tracto2017',
+      },
+    });
 
-       // send mail with defined transport object
     const info = await transporter.sendMail({
-        from: '"Maddison Foo Koch 👻" <halconet@tractozone.com.mx>', // sender address
-        to: "david.cid@tractozone.com.mx", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-      });
-    
-      res.json("envio correcto");
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-   
-    } catch (error) {
-    
-      res.json(error);
+      from: '"Halconet 👻" <halconet@tractozone.com.mx>', // Dirección del remitente
+      to: req.body.correo, // Dirección del destinatario obtenida del cuerpo de la solicitud
+      subject: 'Envio te correo test API', // Asunto
+      text: req.body.envio, // Cuerpo del mensaje en texto plano, obtenido del cuerpo de la solicitud
+      html: `<b>${req.body.envio}</b>`, // Cuerpo del mensaje en HTML, obtenido del cuerpo de la solicitud
+    });
 
-    }
-  
-
-
+    res.json('Envío correcto');
+    console.log('Mensaje enviado: %s', info.messageId);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al enviar el correo electrónico', details: error });
+  }
 };
+
 
 
 
